bd0ec34d64979d2d683853517d0481382f9db3c0
costino0 <albertalexandru006@gmail.com>
2025-01-30 04:55:19 -0500

    Configured login, logout, and register

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // Import the Register component
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} /> {'https://alexandru007-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/register/'}
     </Routes>
   );
 }
+
 export default App;
